d870ca4dac0f47148270089b7032cb23b104a633
Ïvan Hrushevich <ivan.grushevich1@gmail.com>
2021-07-14 10:25:39 +0300

    implement adding Users to list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+	const [usersList, setUsersList] = useState([]);
+
+	const addUserHandler = (name, age) => {
+		setUsersList(prevUsersList => [
+			...prevUsersList,
+			{ name, age, id: Math.random().toString() },
+		]);
+	};
+
 	return (
 		<div className="App">
-			<AddUser />
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
 		</div>
 	);
 }
